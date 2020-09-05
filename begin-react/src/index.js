9d11de307c270eb0b9335a9ab018ec03e9bd63a1
kebele <ykemalbeler@gmail.com>
2020-09-05 23:12:25 +0300

    ekleme

--- begin-react/src/index.js
+++ begin-react/src/index.js
@@ -1,8 +1,6 @@
 // modulleri çağırıyoruz
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-
+import React from "react";
+import ReactDOM from "react-dom";
 
 //component bir class veya function dan oluşan bir kod
 //bir web sayfasındaki her bir şey bir component olarak düşünülebilir
@@ -12,17 +10,51 @@ import ReactDOM from 'react-dom'
 // JSX; js ye html benzeri bir yazım eklenmesi,
 //JSX ifadeleri tek bir div içinde olmalı
 
-const names = ["ronaldo", "sergio", "gareth", "benzema"];
+// const names = ["ronaldo", "sergio", "gareth", "benzema"];
 
+//style="padding: 10px; background-color: red; color : white; border: 2px solid yellow"
+//style yazımında dikkat bir js object şeklinde key:value şeklinde yazılmı var, dikkat
+//className kullanımına dikkat, JSX'de bu şekilde, bazı başka attribute isimleride farklı, bunları yanlış yazıdğımızda bunun için colsole da uyarı alırız,  
+// {...} arasına kodda yazabiliriz, 40-15 mesela, yada aşağıda kodda old. gibi, 
 
-function App(){
-    // return <h1>real madrid</h1>
-    return (
-        <div>
-            {names.map(name => (<h1>{name}</h1>))}
-        </div>
-        // yukarısı JSX yazımı
-    )        
+function App() {
+  // return <h1>real madrid</h1>
+
+  const buttonValue = "benim buton";
+  const str1 = "click "
+  const str2 = "me"
+
+  return (
+    <div>
+      {/* {names.map(name => (<h1>{name}</h1>))} */}
+      <button
+        className="jsxCLASS"
+        type="button"
+        style={{
+          padding: "10px",
+          backgroundColor: "red",
+          color: "white",
+          backgroundColor: "green",
+        }}
+      >
+        {buttonValue}
+      </button>
+      <br/>
+      <button
+        className="jsxCLASS"
+        type="button"
+        style={{
+          padding: "10px",
+          backgroundColor: "red",
+          color: "white",
+          backgroundColor: "green",
+        }}
+      >
+        {str1.concat(str2)}
+      </button>
+    </div>
+    // yukarısı JSX yazımı
+  );
 }
 
 //class based comp yapalım
@@ -32,14 +64,10 @@ function App(){
 //     }
 // }
 
-
 //sayfada göstermek, bunun için render etmemiz lazım
 ReactDOM.render(
-    //neyi göstereceğim, nerede göstereceğim
-    <App/>, 
-    document.getElementById('root')
-    // document.querySelector('#root')
-)
-
-
-
+  //neyi göstereceğim, nerede göstereceğim
+  <App />,
+  document.getElementById("root")
+  // document.querySelector('#root')
+);
