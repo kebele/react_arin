a3ef31db0700701a157f928c43f9a5d105cea3f3
kebele <ykemalbeler@gmail.com>
2020-09-05 22:14:06 +0300

    ekleme

--- begin-react/src/index.js
+++ begin-react/src/index.js
@@ -9,23 +9,37 @@ import ReactDOM from 'react-dom'
 //element comp un en küçük yapı taşı
 
 //functional comp yapalım
+// JSX; js ye html benzeri bir yazım eklenmesi,
+//JSX ifadeleri tek bir div içinde olmalı
 
-// function App(){
-//     return <h1>real madrid</h1>
-// }
+const names = ["ronaldo", "sergio", "gareth", "benzema"];
+
+
+function App(){
+    // return <h1>real madrid</h1>
+    return (
+        <div>
+            {names.map(name => (<h1>{name}</h1>))}
+        </div>
+        // yukarısı JSX yazımı
+    )        
+}
 
 //class based comp yapalım
-class App extends React.Component {
-    render (){
-        return <h1>barcelona</h1>
-    }
-}
+// class App extends React.Component {
+//     render (){
+//         return <h1>barcelona</h1>
+//     }
+// }
 
 
 //sayfada göstermek, bunun için render etmemiz lazım
 ReactDOM.render(
     //neyi göstereceğim, nerede göstereceğim
-    <App/>, document.getElementById('root')
+    <App/>, 
+    document.getElementById('root')
+    // document.querySelector('#root')
 )
+
 
 
